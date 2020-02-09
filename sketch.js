ec1351c1b5c84072e77ca2a6fdcd0187a8482229
ruhi97 <59785533+ruhi97@users.noreply.github.com>
2020-02-09 08:48:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+// You could have multiple flags like: start, launch to indicate the state of the game.
+
+const { Engine, World, Bodies, Body, Mouse, MouseConstraint, Constraint, Composite, Detector } = Matter;
+// The above line creates different constant variables for Engine, World, Bodies etc.
+
+var ball(10,20,100,100);
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+const {Engine} = Matter 
+is the same as c
+onst Engine = Matter.Engine
+
+*/
+
+function setup() {
+    // Setup the canvas, the ground the, tanker, the shooting ball and the bubble balls.
+
+}
+
+function draw() {
+// Remember to update the Matter Engine and set the background.
+}
+
+
+function keyReleased() {
+    // Call the shoot method for the cannon.
+}
